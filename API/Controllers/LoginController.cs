63195ff5b0bf428273a26f7feeaef5637bb64019
tuananh603787 <67224723+seriousman1220@users.noreply.github.com>
2023-04-11 17:44:05 +0700

    -add

--- API/Controllers/LoginController.cs
+++ API/Controllers/LoginController.cs
@@ -5,7 +5,8 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-
+using Npgsql;
+using System.Data;
 
 namespace API.Controllers
 {
@@ -14,6 +15,17 @@ namespace API.Controllers
         [HttpPost]
         public HttpResponseMessage CheckLogin(LoginInfo account)
         {
+            NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;Port=5432;Database=postgres;User Id=postgres;Password=seriousman");
+            conn.Open();
+            NpgsqlCommand query = new NpgsqlCommand();
+            query.Connection = conn;
+            query.CommandType = CommandType.Text;
+            query.CommandText = "select * from dmnhvt";
+            NpgsqlDataAdapter nda = new NpgsqlDataAdapter(query);
+            DataTable dt = new DataTable();
+            nda.Fill(dt);
+            query.Dispose();
+            conn.Close();
             if (account.username.ToString() == "admin" && account.password.ToString() == "admin")
                 return Request.CreateResponse(HttpStatusCode.OK, "OK");
             else
